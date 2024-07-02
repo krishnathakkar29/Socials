05f3e8ff87723eb4d576dfb4f4b10a403c1712a1
Krishna Thakkar <work.krishnat@gmail.com>
2024-07-02 13:31:09 +0530

    backendurl

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const BACKEND_URL =  import.meta.env.VITE_BACKEND_URL 
+// const BACKEND_URL =  import.meta.env.VITE_BACKEND_URL 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy:{
       "/api": {
-        target: BACKEND_URL,
+        target: import.meta.env.VITE_BACKEND_URL,
         changeOrigin: true
       }
     }
