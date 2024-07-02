98e531af5d07c271658746250448bb5d3b6765ac
Krishna Thakkar <work.krishnat@gmail.com>
2024-07-02 13:35:53 +0530

    url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy:{
       "/api": {
-        target: import.meta.env.VITE_BACKEND_URL,
+        target: 'http://localhost:5000',
         changeOrigin: true
       }
     }
